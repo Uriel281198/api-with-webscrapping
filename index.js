f8fb8c74255986080851714dc317463f9b22a6af
Uriel281198 <urielarriaga.1998@gmail.com>
2020-11-21 19:52:36 -0600

    add port

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const {app} = require('./app');
 
-app.listen(4000)
+app.listen(process.env.PORT || 4000)
 console.log("Server On port 4000")
 
+
+/*
+git update-ref -d HEAD
+git push origin master -f
+*/
